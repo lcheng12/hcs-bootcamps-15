766c67b16906312e711d623013245e69e33100c4
Lucy Cheng <lcheng@college.harvard.edu>
2015-09-13 10:16:54 -0400

    Bootcamp 0 Code

---
+++ bc0/lists.py
@@ -0,0 +1,17 @@
+i_like_pizza = True
+
+lst1 = ["Javier", 19, True]
+lst2 = [1,2,3,4]
+lst3 = ["what", "is", "up"]
+
+if i_like_pizza:
+    for item in range(9):
+        print ("pizza")
+
+for item in lst1:
+    print (lst1[0])
+
+for item in lst2:
+    item += 1
+
+print (lst2)
---
+++ bc0/print.py
@@ -0,0 +1,14 @@
+num = 10
+decimal = 1.5
+boolean = True
+string = "Welcome to HCS!"
+
+new_num = num * 10
+
+print (num)
+print (decimal)
+print (boolean)
+print (new_num)
+print (string)
+
+print("Python ain't too bad")
